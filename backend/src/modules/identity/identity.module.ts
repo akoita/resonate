6bd474d2e346ef17862080cda34fa3c2b331500e
akoita <aboubakar.koita@yahoo.fr>
2026-01-18 15:44:26 +0100

    Improve wallet funding and budget events

--- backend/src/modules/identity/identity.module.ts
+++ backend/src/modules/identity/identity.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { EventBus } from "../shared/event_bus";
 import { WalletController } from "./wallet.controller";
 import { WalletService } from "./wallet.service";
 
 @Module({
   controllers: [WalletController],
-  providers: [WalletService],
+  providers: [EventBus, WalletService],
   exports: [WalletService],
 })
 export class IdentityModule {}
